307d1a28d480e2e6c96633f0a52cf14fe93989cd
hasan <hasankiwan.action@gmail.com>
2022-04-14 12:10:49 +0300

    s

--- app/Http/Requests/RegistrationRequest.php
+++ app/Http/Requests/RegistrationRequest.php
@@ -29,7 +29,7 @@ class RegistrationRequest extends \App\Http\Requests\Api\FormRequest
             'name' => ['required', 'string', 'max:255'],
             'email' => ['string', 'email', 'max:255', 'unique:users'],
             'phone' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password' => ['required', 'string', 'min:8'],
         ];
 
     }
