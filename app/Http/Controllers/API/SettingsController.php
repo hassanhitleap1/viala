de1352e2d72c5e69002f4803996ef6029823ce99
hasan <hasankiwan.action@gmail.com>
2022-04-17 22:28:47 +0300

    sdas

--- app/Http/Controllers/API/SettingsController.php
+++ app/Http/Controllers/API/SettingsController.php
@@ -12,21 +12,38 @@ class SettingsController extends Controller
     public function terms_and_conditions(){
 
         $settings= Settings::first();
-        return response()->json([$settings->terms_and_conditions]);
+        $data=[
+            'body_en'=>  $settings->terms_and_conditions_en ,
+            'body_ar'=>  $settings->terms_and_conditions_ar ,
+            'body_he'=>  $settings->terms_and_conditions_he ,
+            ];
+        return response()->json( $data);
     }
 
 
     public function about(){
 
         $settings= Settings::first();
-        return response()->json([$settings->about]);
+        $data=[
+            'body_en'=>  $settings->about_en ,
+            'body_ar'=>  $settings->about_ar ,
+            'body_he'=>  $settings->about_he ,
+            ];
+        return response()->json( $data);
+      
     }
 
 
     public function privacy_policy(){
 
         $settings= Settings::first();
-        return response()->json([$settings->privacy_policy]);
+        $data=[
+            'body_en'=>  $settings->privacy_policy_en ,
+            'body_ar'=>  $settings->privacy_policy_ar ,
+            'body_he'=>  $settings->privacy_policy_he ,
+            ];
+        return response()->json( $data);
+      
     }
 
 
