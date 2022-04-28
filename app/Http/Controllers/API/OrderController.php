2d5eff87886b872f7b243db7b78e172dad907455
hassan <hassanhitleap1@gmail.com>
2022-04-28 21:07:53 +0300

    20

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -20,7 +20,7 @@ class OrderController extends Controller
 
     public function __construct()
     {
-           $this->middleware('jwt.verify')->only(['index','store','update','show','booking_history','destroy']);
+           $this->middleware('jwt.verify')->only(['index','store','update','show','booking_history','previous_booking','destroy']);
     }
 
 
@@ -38,6 +38,16 @@ class OrderController extends Controller
         // return VailaResource::collection(Vaila::whereIn('id',function($q){
         //     $q->select('vaial_id')->from( 'orders')
         // })->paginate(10));
+    }
+
+
+
+    public function previous_booking(){
+        $orders =Vaila::join('orders','orders.vaial_id','vaila.id')
+            ->where( 'vaila.user_id',auth('api-jwt')->user()->id)
+            ->paginate(10);
+        return BookingHistoryResource::collection($orders);
+    
     }
 
     public function index(){
@@ -91,17 +101,52 @@ class OrderController extends Controller
     }
 
 
+    
+
     public function book_naw(OrderRequest  $request){
 
-        $order= Orders::create([
+        $order =Orders::where('vaial_id',$request->vaial_id)->whereBetween('form_date', [$request->form_date, $request->to_date])->get();
+        $vaial=Vaila::find($request->vaial_id);
+
+        if($order->count()){
+            $form_date=     date("Y-m-d", strtotime($order[0]->form_date)); 
+            $to_date= date("Y-m-d", strtotime($order[0]->to_date)); 
+            return response()->json([
+                'success'=>false,
+                "message" => "this vaial not available booked from $form_date to $to_date",
+                'errors' => [],
+                'status' => 422,
+                'data'=>$vaial
+            ], 422);
+        }
+
+        $order =Orders::create([
             'form_date'=>$request->form_date,
             'to_date'=>$request->to_date,
-            'phone'=>$request->phone,
-            'type'=>$request->price,
-            'vaial_id'=>$request->vaial_id
+            'price'=>$request->price,
+            'payment_type'=>$request->payment_type,
+            'vaial_id'=>$request->vaial_id,
+            'user_id'=> auth('api-jwt')->user()->id
         ]);
+       // $calculation=AccountingHelper::calculation_order($request->price);
 
+        // Accounting::create([
+        //     'for_me'=>$calculation['for_app'],
+        //     'for_app'=>$calculation['for_app'],
+        //     'user_id'=>auth('api-jwt')->user()->id
+        // ]);
+        $viala=Vaila::find($request->vaial_id);
+        $viala->number_booking= $viala->number_booking + 1;
+        $viala->save();
+        //MakeOrderEvent::dispatch($order);
+        $order['merchant']=$order->merchant;
+        return response()->json([
+            'success'=>true,
+            'status' => 201,
+            'data'=>$order
+        ], 422);
 
+        
         return new OrdersResource($order);
 
     }
--- routes/api.php
+++ routes/api.php
@@ -38,6 +38,7 @@ Route::get('vaila/nearby', [\App\Http\Controllers\API\VailaController::class, 'n
 Route::apiResource('vaila',\App\Http\Controllers\API\VailaController::class);
 
 Route::get('booking-history',[\App\Http\Controllers\API\OrderController::class,'booking_history']);
+Route::get('previous-booking',[\App\Http\Controllers\API\OrderController::class,'previous_booking']);
 Route::apiResource('orders',\App\Http\Controllers\API\OrderController::class);
 Route::apiResource('comments',\App\Http\Controllers\API\CommentsController::class);
 Route::apiResource('rates',\App\Http\Controllers\API\RateController::class);
