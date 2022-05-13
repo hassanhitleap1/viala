f354a198a875d80f0745bd3302b3c5dbb0b5ef29
hassan <hassanhitleap1@gmail.com>
2022-05-13 04:31:45 +0300

    :

--- app/Http/Controllers/AuthJwt/AuthController.php
+++ app/Http/Controllers/AuthJwt/AuthController.php
@@ -270,11 +270,14 @@ class AuthController extends Controller
         $user->save();
         $data = array('name'=>"Virat Gandhi");
         
-        Mail::send(['text'=>'mail'], $data, function($message,$user) {
-            $message->to($user->email , 'new password is pass@123')->subject
+      
+
+         Mail::send(['text'=>'mail'], $data, function($message) use($user){
+            $message->to($user->email, 'new password is pass@123')->subject
                ('Laravel Basic Testing Mail');
-            $message->from(env('MAIL_USERNAME'),'tasheh');
+               $message->from(env('MAIL_USERNAME'),'tasheh');
          });
+
 
          return response()->json([
             'success' => true,
